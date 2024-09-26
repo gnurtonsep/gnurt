ab4d44cf5fada80dd0d34b02c2498fe0dd27bb03
jt <gnurtonsep@gmail.com>
2024-09-26 17:25:50 +0700

    update: Update button component for color style, padding style, update press state and props in button component

--- ui/src/components/Button/Button.tsx
+++ ui/src/components/Button/Button.tsx
@@ -1,11 +1,19 @@
-import { ComponentProps, MouseEvent, useCallback, useState } from "react";
+import { ComponentProps, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
-import { Color } from "@/utils";
+import { Color, FontSize } from "@/utils";
+
+import {
+    BUTTON_CONTAINED_COLOR,
+    BUTTON_LIGHT_COLOR,
+    BUTTON_OUTLINED_COLOR,
+} from "./styles";
 
 /**
  * ========== Define type ==========
  */
+type ButtonVariant = "contained" | "light" | "outlined";
+
 type ButtonTextTransform =
     | "uppercase"
     | "lowercase"
@@ -14,6 +22,9 @@ type ButtonTextTransform =
 
 type ButtonProps = ComponentProps<"button"> & {
     /* Display UI */
+    variant?: ButtonVariant;
+    color?: Color;
+    size?: FontSize;
     fullWidth?: boolean;
 
     /* Display content */
@@ -42,14 +53,29 @@ const generateButtonTextTransform = (
     }
 };
 
+const generateButtonColor = (variant: ButtonVariant, color: Color) => {
+    switch (variant) {
+        case "contained":
+            return BUTTON_CONTAINED_COLOR[color];
+        case "light":
+            return BUTTON_LIGHT_COLOR[color];
+        case "outlined":
+            return BUTTON_OUTLINED_COLOR[color];
+        default:
+            return BUTTON_CONTAINED_COLOR[color];
+    }
+};
+
 const Button = ({
     className,
+    variant = "contained",
+    size = "base",
+    color = "gray",
     fullWidth,
     text,
     textTransform = "first-uppercase",
     isLoading,
-    textIsLoading,
-    onClick,
+    textIsLoading = "Loading",
     ...props
 }: ButtonProps) => {
     /**
@@ -57,27 +83,15 @@ const Button = ({
      */
     const [isPressed, setIsPressed] = useState<boolean | undefined>();
 
-    /* Define handle press */
-    const handlePress = useCallback(
-        (event: MouseEvent<HTMLButtonElement>) => {
-            setIsPressed(true);
-
-            if (onClick) {
-                onClick(event);
-            }
-
-            setIsPressed(undefined);
-        },
-        [onClick]
-    );
-
     return (
         <button
             {...props}
             className={twMerge(
                 "inline-flex items-center justify-center gap-2 appearance-none outline-none transition-all",
                 generateButtonTextTransform(textTransform),
-                "focus-within:scale-90",
+                generateButtonColor(variant, color),
+                "px-4 py-2 font-medium leading-none",
+                "data-[pressed=true]:scale-90 rounded-lg",
                 fullWidth ? "w-full" : "",
                 "group",
                 className
@@ -85,8 +99,20 @@ const Button = ({
             disabled={(isLoading ? isLoading : undefined) || props.disabled}
             data-disable={isLoading ? isLoading : undefined}
             data-loading={isLoading ? isLoading : undefined}
+            data-text={size}
             data-pressed={isPressed}
-            onClick={handlePress}
+            onMouseDown={(e) => {
+                setIsPressed(true);
+                props.onMouseDown?.(e);
+            }}
+            onMouseUp={(e) => {
+                setIsPressed(false);
+                props.onMouseUp?.(e);
+            }}
+            onMouseLeave={(e) => {
+                setIsPressed(false);
+                props.onMouseLeave?.(e);
+            }}
         >
             <span>{isLoading ? `${textIsLoading}...` : text}</span>
         </button>
